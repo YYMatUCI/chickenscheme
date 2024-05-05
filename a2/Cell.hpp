eec00f0c78ec2839b8ed862593133935e93deead
RogerMin <rogermingaming@gmail.com>
2024-05-05 15:29:07 -0700

    Initial a2 files

---
+++ a2/Cell.hpp
@@ -0,0 +1,21 @@
+/**
+ * \mainpage COMP2012H Programming Assignment 2, Fall 2014
+ *
+ * \author **Put your name here**
+ * \author **Your studentID**
+ * \author **Your email**
+ * \author **Your lab session**
+ *
+ * \date **Fill in the date**
+ *
+ * Instructor: <a href="http://www.cs.ust.hk/~dekai/">Dekai Wu</a>
+ * Due: 2014.10.10 at 23:00 by CASS
+ */
+
+/**
+ * \file Cell.hpp
+ *
+ * Encapsulates the abstract interface for a concrete class-based
+ * implementation of cells for a cons list data structure.
+ */
+
