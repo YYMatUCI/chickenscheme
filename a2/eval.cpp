5db9595591928897c3024216a42ad0775f3a52a2
Roger Min <yiyangm2@uci.edu>
2024-05-06 09:40:26 -0700

    a2 finished step3&4, support -,*,/, quote operands

--- a2/eval.cpp
+++ a2/eval.cpp
@@ -12,18 +12,87 @@
 /**
  * \brief Evaluate plus cell.
  * \param c Head of cells to plus.
+ * \param is_minus True if would like to calculate minus.
  * \return A constant cell which contains int/double. 0 if no parameters given.
  */
-Cell* eval_plus(Cell* const c)
+Cell* eval_plus(Cell* const c, bool is_minus=false)
 {
+  if (is_minus && (nullp(c) || nullp(cdr(c)))) {
+    cerr << "ERROR: At least two parameters are needed for minus operator.\n";
+    exit(1);
+  }
   bool is_int = true;
   double d = 0;
   Cell* cur = c;
+  // Case minus would be a - b - c - ... = - (-a + b + c + ...)
+  if (is_minus) {
+    eval(car(cur))->plus_c(is_int, d);
+    cur = cdr(cur);
+    d = -d;
+  }
   while (!nullp(cur)) {
     eval(car(cur))->plus_c(is_int, d);
     cur = cdr(cur);
   }
+  if (is_minus) {
+    d = -d;
+  }
   return make_num(is_int, d);
+}
+
+/**
+ * \brief Evaluate minus cell.
+ * \param c Head of cells to minus.
+ * \return A constant cell which contains int/double.
+ */
+Cell* eval_minus(Cell* const c)
+{
+  return eval_plus(c, true);
+}
+
+/**
+ * \brief Evaluate multi cell. Early stopping when 0 founded.
+ * \param c Head of cells to multi.
+ * \param is_divide
+ * \return A constant cell which contains int/double. 1 if no parameters given.
+ */
+Cell* eval_multi(Cell* const c, bool is_divide=false)
+{
+  if (is_divide && (nullp(c) || nullp(cdr(c)))) {
+    cerr << "ERROR: At least two parameters are needed for minus operator.\n";
+    exit(1);
+  }
+  bool is_int = true;
+  double d = 1;
+  double n = 1;
+  Cell* cur = c;
+  if (is_divide) {
+    eval(car(cur))->multi_c(is_int, n);
+    cur = cdr(cur);
+  }
+  while (!nullp(cur)) {
+    eval(car(cur))->multi_c(is_int, d);
+    if (d == 0) break;
+    cur = cdr(cur);
+  }
+  if (is_divide) {
+    if (d == 0) {
+      cerr << "ERROR: The divisor cannot be zero.\n";
+      exit(1);
+    }
+    d = n / d;
+  }
+  return make_num(is_int, d);
+}
+
+/**
+ * \brief Evaluate divide cell.
+ * \param c Head of cells to divide.
+ * \return A constant cell which contains int/double.
+ */
+Cell* eval_divide(Cell* const c)
+{
+  return eval_multi(c, true);
 }
 
 /**
@@ -94,6 +163,20 @@ Cell* eval_if(Cell* const c)
 }
 
 /**
+ * \brief Evalute quote.
+ * \param c Quote cell.
+ * \return The car of c's cdr.
+*/
+Cell* eval_quote(Cell* const c)
+{
+  if (nullp(c) || !nullp(cdr(c))) {
+    cerr << "ERROR: Exactly one parameter is needed for quote.\n";
+    exit(1);
+  }
+  return car(c);
+}
+
+/**
  * \brief Evaluate cell c.
  * \param c The evaluated cell.
  * \return A constant cell which contains int/double.
@@ -105,10 +188,20 @@ Cell* eval(Cell* const c)
     string s = get_symbol(eval(car(c)));
     if (s == "+") {
       cell = eval_plus(cdr(c));
+    } else if (s == "-") {
+      cell = eval_minus(cdr(c));
+    } else if (s == "*") {
+      cell = eval_multi(cdr(c));
+    } else if (s == "/") {
+      cell = eval_divide(cdr(c));
     } else if (s == "ceiling") {
       cell = eval_ceiling(cdr(c));
-    }  else if (s == "if") {
+    } else if (s == "floor") {
+      cell = eval_floor(cdr(c));
+    } else if (s == "if") {
       cell = eval_if(cdr(c));
+    } else if (s == "quote") {
+      cell = eval_quote(cdr(c));
     } else {
       cerr << "ERROR: key word '" << s << "' not supported yet.\n";
       exit(1);
